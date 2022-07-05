14641013ff4c74b7c6f241b9c1b61bf1004b11c6
Ramil3107 <106266508+Ramil3107@users.noreply.github.com>
2022-07-05 03:56:49 +0300

    add input form

--- src/components/content/Almanac/HtmlAlmanac/HtmlAlmanac.jsx
+++ src/components/content/Almanac/HtmlAlmanac/HtmlAlmanac.jsx
@@ -2,26 +2,33 @@ import React, { useState } from "react";
 import styles from "./HtmlAlmanac.module.css"
 
 
-const data = [{
-    question: "Что такое HTML?",
-    answer: "HTML (от англ. HyperText Markup Language — «язык гипертекстовой разметки») — стандартизированный язык гипертекстовой разметки документов для просмотра веб-страниц в браузере. Веб-браузеры получают HTML документ от сервера по протоколам HTTP/HTTPS или открывают с локального диска, далее интерпретируют код в интерфейс, который будет отображаться на экране монитора."
-},
-{
-    question: "Что такое HTML?",
-    answer: "HTML (от англ. HyperText Markup Language — «язык гипертекстовой разметки») — стандартизированный язык гипертекстовой разметки документов для просмотра веб-страниц в браузере. Веб-браузеры получают HTML документ от сервера по протоколам HTTP/HTTPS или открывают с локального диска, далее интерпретируют код в интерфейс, который будет отображаться на экране монитора."
-},
-{
-    question: "Что такое HTML?",
-    answer: "HTML (от англ. HyperText Markup Language — «язык гипертекстовой разметки») — стандартизированный язык гипертекстовой разметки документов для просмотра веб-страниц в браузере. Веб-браузеры получают HTML документ от сервера по протоколам HTTP/HTTPS или открывают с локального диска, далее интерпретируют код в интерфейс, который будет отображаться на экране монитора."
-}
-]
-
-
-
 
 
 
 function HtmlAlmanac(props) {
+
+    const [data,setData] = useState ([{
+        question: "Что такое HTML?",
+        answer: "HTML (от англ. HyperText Markup Language — «язык гипертекстовой разметки») — стандартизированный язык гипертекстовой разметки документов для просмотра веб-страниц в браузере. Веб-браузеры получают HTML документ от сервера по протоколам HTTP/HTTPS или открывают с локального диска, далее интерпретируют код в интерфейс, который будет отображаться на экране монитора."
+    },
+    {
+        question: "Что такое HTML?",
+        answer: "HTML (от англ. HyperText Markup Language — «язык гипертекстовой разметки») — стандартизированный язык гипертекстовой разметки документов для просмотра веб-страниц в браузере. Веб-браузеры получают HTML документ от сервера по протоколам HTTP/HTTPS или открывают с локального диска, далее интерпретируют код в интерфейс, который будет отображаться на экране монитора."
+    },
+    {
+        question: "Что такое HTML?",
+        answer: "HTML (от англ. HyperText Markup Language — «язык гипертекстовой разметки») — стандартизированный язык гипертекстовой разметки документов для просмотра веб-страниц в браузере. Веб-браузеры получают HTML документ от сервера по протоколам HTTP/HTTPS или открывают с локального диска, далее интерпретируют код в интерфейс, который будет отображаться на экране монитора."
+    }
+    ])
+    
+    const [question, setQstn] = useState('')
+    const [answer, setAnswr] = useState('')
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const faqs = {question,answer}
+        setData(data => data.concat(faqs))
+        console.log(faqs)
+    }
 
     const [selected, setSelected] = useState(null)
 
@@ -32,9 +39,20 @@ function HtmlAlmanac(props) {
         setSelected(i)
     }
 
+
     return (
 
         <div className={styles.wrapper}>
+
+            <form onSubmit={handleSubmit}>
+                <label>Question</label>
+                <input type="text" required value={question} onChange={(e) => setQstn(e.target.value)}/>
+
+                <label>Answer</label>
+                <input type="text" required value={answer} onChange={(e) => setAnswr(e.target.value)}/>
+
+                <button>Add FAQ</button>
+            </form>
 
             <div className={styles.accordion}>
                 {data.map((item, i) => (
