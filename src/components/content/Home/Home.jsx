9834f941bbabddad2456d40462c8b4f1a24c837c
Ramil3107 <106266508+Ramil3107@users.noreply.github.com>
2022-07-10 23:11:32 +0300

    move slideshow to archive

--- src/components/content/Home/Home.jsx
+++ src/components/content/Home/Home.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import styles from "./Home.module.css"
 import Text from "./Text/Text";
-import Slideshow from "./Slideshow/Slideshow";
+
 
 function Home(props) {
     return (
         <div className={styles.wrapper}>
             <Text />
-            <Slideshow />
         </div>
     )
 }
+
 
 export default Home;
