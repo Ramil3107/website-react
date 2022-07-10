70d1ffe9b5d1b3c1c23a552e10b285c89493174e
Ramil3107 <106266508+Ramil3107@users.noreply.github.com>
2022-07-11 02:28:37 +0300

    add new styles and app bar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,80 @@
+import { AppBar, Avatar, Button, Menu, MenuItem, Toolbar, Typography } from "@mui/material";
 import React from "react";
-import { NavLink, Router } from "react-router-dom";
+import { Link, NavLink, } from "react-router-dom";
 import styles from "./Navbar.module.css"
 
 
 function Navbar(props) {
+
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
     return (
         <div className={styles.wrapper}>
 
-            <div className={styles.item}>
+            <AppBar position="absolute" color="default">
+                <Toolbar sx={{ display: "flex", justifyContent: "space-between", }}>
+
+                    <Typography
+                        variant="h6"
+                        component={Link}
+                        to="/website-react"
+                        noWrap
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'none', md: 'flex' },
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.1rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }} >
+                        REACT APP
+                    </Typography>
+                    <Button component={Link} to="/website-react" variant="text" color="inherit"> Home</Button>
+                    <Button component={Link} to="/almanac" variant="text" color="inherit">Almanac</Button>
+                    <Button component={Link} to="/todo" variant="text" color="inherit">ToDo List</Button>
+                    <Button component={Link} to="/contacts" variant="text" color="inherit">Contacts</Button>
+                    {/* <Button component={Link} to="/login" variant="text" color="inherit">Log In</Button> */}
+
+                    <Avatar
+                        id="avatar"
+                        aria-controls={open ? 'menu' : undefined}
+                        aria-haspopup="true"
+                        aria-expanded={open ? 'true' : undefined}
+                        onClick={handleClick}
+                    >
+                        R
+                    </Avatar>
+
+                    <Menu
+                        id="menu"
+                        aria-labelledby="avatar"
+                        anchorEl={anchorEl}
+                        open={open}
+                        onClose={handleClose}
+                        anchorOrigin={{
+                            vertical: 'top',
+                            horizontal: 'left',
+                        }}
+                        transformOrigin={{
+                            vertical: 'top',
+                            horizontal: 'left',
+                        }}
+                    >
+                        <MenuItem component={Link} to="/login" onClick={handleClose}>Log In</MenuItem>
+                        <MenuItem component={Link} to="/login" onClick={handleClose}>Sign In</MenuItem>
+                    </Menu>
+                </Toolbar>
+            </AppBar>
+
+            {/* <div className={styles.item}>
                 <NavLink to="/website-react" className={({ isActive }) => (isActive ? [styles.active] : [])}>Home</NavLink>
             </div>
 
@@ -25,7 +92,7 @@ function Navbar(props) {
 
             <div className={styles.login}>
                 <NavLink to="/login" className={({ isActive }) => (isActive ? [styles.active] : [])}>Login</NavLink>
-            </div>
+            </div> */}
 
         </div>
     )
